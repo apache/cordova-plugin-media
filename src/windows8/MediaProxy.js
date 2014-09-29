bf42b0852cbee6c15eb168beba0253ddcdd9c50c
jarivas <joseantonio.rivasfernandez@gmail.com>
2014-09-29 13:28:21 +0200

    make possible plays wav file

--- src/windows8/MediaProxy.js
+++ src/windows8/MediaProxy.js
@@ -42,7 +42,7 @@ module.exports = {
 
         var fn = src.split('.').pop(); // gets the file extension
         if (thisM.node === null) {
-            if (fn === 'mp3' || fn === 'wma' || fn === 'wma' ||
+            if (fn === 'mp3' || fn === 'wma' || fn === 'wav' ||
                 fn === 'cda' || fn === 'adx' || fn === 'wm' ||
                 fn === 'm3u' || fn === 'wmx' || fn === 'm4a') {
                 thisM.node = new Audio(src);
