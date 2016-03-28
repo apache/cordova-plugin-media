b1cec811dafb965d371dae7751657b540df03945
Federico Hatoum <fhatoum@grassroots-tech.com>
2016-03-28 17:31:06 -0400

    Tracking audio level reporting out of native into JS/Angular

--- www/Media.js
+++ www/Media.js
@@ -54,7 +54,8 @@ var Media = function(src, successCallback, errorCallback, statusCallback, meteri
     this._position = -1;
     
     console.log('Metering enabled is: ' + this.isMeteringEnabled);
-    exec(this.successCallback, this.errorCallback, "Media", "create", [this.id, this.src, this.isMeteringEnabled]);
+    //exec(this.successCallback, this.errorCallback, "Media", "create", [this.id, this.src, this.isMeteringEnabled]);
+    exec(this.successCallback, this.errorCallback, "Media", "create", [this.id, this.src, true]);
 };
 
 // Media messages
