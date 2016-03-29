4df37fa49f4c1258907a07bdd455ccebbbed37f7
Federico Hatoum <fhatoum@grassroots-tech.com>
2016-03-29 11:41:42 -0400

    Adjusting audio level conversion values

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -482,14 +482,10 @@
             
             
             NSDictionary* recordSettings = [NSDictionary dictionaryWithObjectsAndKeys:
-                                            [NSNumber numberWithInt:AVAudioQualityMedium],
-                                            AVEncoderAudioQualityKey,
-                                            [NSNumber numberWithInt:16],
-                                            AVEncoderBitRateKey,
-                                            [NSNumber numberWithInt: 2],
-                                            AVNumberOfChannelsKey,
-                                            [NSNumber numberWithFloat:44100.0],
-                                            AVSampleRateKey,
+                                            [NSNumber numberWithInt:AVAudioQualityMedium], AVEncoderAudioQualityKey,
+                                            [NSNumber numberWithInt:16], AVEncoderBitRateKey,
+                                            [NSNumber numberWithInt: 1], AVNumberOfChannelsKey,
+                                            [NSNumber numberWithFloat:44100.0], AVSampleRateKey,
                                             nil];
             // create a new recorder for each start record
             audioFile.recorder = [[CDVAudioRecorder alloc] initWithURL:audioFile.resourceURL
@@ -851,12 +847,15 @@
     // userInfo is ref to AVAudioRecorder or AVAudioPlayer
     NSNumber* audioLevel = [self generateAudioLevel: timer.userInfo];
     
-    // convert audioLevel, which is reported in dB from -160 dB to 0 dB, to an integer value from 0 to 100.
+    // convert audioLevel, which is reported in dB from -80 dB to 0 dB, to an integer value from 0 to 100.
+    // (NOTE: AVAudioPlayer docs say minimum audio level is -160 dB, but in practice I have found
+    // the minimum to be closer to -80 dB and have seen supporting opinions on the interwebs.
+    
     // audioLevel * -1 converts the number to positive value
     // 100/160 generates a scaling factor (0.625 in this case)
     // positive audioLevel * scaling factor = audio level value in terms of 0-100
     // Subtract the scaled audio level from the max audio level of 100, since the dB scaled is inversed
-    int scaledAudioLevel = 100 - (([audioLevel intValue] * -1) * 100/160);
+    int scaledAudioLevel = 100 - (([audioLevel intValue] * -1) * 100/74);
     
     NSLog(@"iOS: Raw Audio Level   : %@", audioLevel);
     NSLog(@"iOS: Scaled Audio Level: %@", [NSNumber numberWithInt: scaledAudioLevel]);
