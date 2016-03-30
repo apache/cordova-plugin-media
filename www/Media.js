9621ba11f203b33ca8fc1c18ebd90a7ef48b086f
Federico Hatoum <fhatoum@grassroots-tech.com>
2016-03-30 14:17:25 -0400

    Fixing isMetering value

--- www/Media.js
+++ www/Media.js
@@ -49,7 +49,7 @@ var Media = function(src, successCallback, errorCallback, statusCallback, meteri
     this.errorCallback = errorCallback;
     this.statusCallback = statusCallback;
     this.meteringCallback = meteringCallback;
-    this.isMeteringEnabled = (meteringCallback) ? true : false;
+    this.isMeteringEnabled = (typeof meteringCallback === 'function') ? true : false;
     this._duration = -1;
     this._position = -1;
     
