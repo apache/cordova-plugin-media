d7e8dfdbc2d3cd5916f926b665bde1b6bc93f9f4
Nico <stream.msn@gmail.com>
2019-03-28 09:31:09 -0300

    Cherry Pick aeaaf90196514cb06b0df8188e1dda7551ec8673

--- www/Media.js
+++ www/Media.js
@@ -64,7 +64,8 @@ Media.MEDIA_STARTING = 1;
 Media.MEDIA_RUNNING = 2;
 Media.MEDIA_PAUSED = 3;
 Media.MEDIA_STOPPED = 4;
-Media.MEDIA_MSG = ['None', 'Starting', 'Running', 'Paused', 'Stopped'];
+Media.MEDIA_FINISHED = 5;
+Media.MEDIA_MSG = ["None", "Starting", "Running", "Paused", "Stopped", "Finished"];
 
 // "static" function to return existing objs.
 Media.get = function (id) {
