90cfe8531325eca9a5c5802e9b2868fd460af934
Victor Andrade <victor.andrade62@hotmail.com>
2020-10-27 11:55:27 -0300

    Update imports file AudioPlayer.java

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -25,6 +25,7 @@ import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.os.Build;
 
 import org.apache.cordova.LOG;
 
