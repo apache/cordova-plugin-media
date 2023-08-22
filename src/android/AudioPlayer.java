957249b6b3f160416c8713a6fd331189052b6c72
エリス <erisu@users.noreply.github.com>
2023-08-22 23:36:35 +0900

    feat(android): increase audio encoding bitrate and sampling rate (#383)

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -170,6 +170,8 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
             this.recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             this.recorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS); // RAW_AMR);
             this.recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC); //AMR_NB);
+            this.recorder.setAudioEncodingBitRate(96000);
+            this.recorder.setAudioSamplingRate(44100);
             this.tempFile = createAudioFilePath(null);
             this.recorder.setOutputFile(this.tempFile);
             try {
